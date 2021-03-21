a8ebaa50157cc07d96eb1eab6a4bab27d0fed748
srg <skabass@yandex.ru>
2021-03-21 23:17:25 +0300

    Добавлет возможность устанавливать фильтры с помощью noUiSlider

--- js/img-upload.js
+++ js/img-upload.js
@@ -1,8 +1,37 @@
 import {isEscEvent} from './utils.js';
+import '../nouislider/nouislider.js';
 
 const DEFAULT_SCALE = 100;
 const MIN_SCALE = 25;
 const STEP_SCALE = 25;
+
+const FilterOptions = {
+  Chrome: {
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1,
+  },
+  Sepia: {
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1,
+  },
+  Marvin: {
+    MIN: 0,
+    MAX: 100,
+    STEP: 1,
+  },
+  Phobos: {
+    MIN: 0,
+    MAX: 3,
+    STEP: 0.1,
+  },
+  Heat: {
+    MIN: 1,
+    MAX: 3,
+    STEP: 0.1,
+  },
+};
 
 const uploadElement = document.querySelector('.img-upload');
 // const form = uploadElement.querySelector('.img-upload__form');
@@ -15,6 +44,63 @@ const scaleElements = uploadElement.querySelector('.img-upload__scale');
 const btnScaleDown = scaleElements.querySelector('.scale__control--smaller');
 const btnScaleUp = scaleElements.querySelector('.scale__control--bigger');
 const inputScale = scaleElements.querySelector('.scale__control--value');
+
+const effectsElements = uploadElement.querySelector('.img-upload__effects');
+const inputEffect = uploadElement.querySelector('.effect-level__value');
+const slider = uploadElement.querySelector('.effect-level__slider');
+
+const sliderUpdateHandler = (values, handle) => {
+  const value = parseFloat(values[handle]);
+  inputEffect.value = value;
+  photo.style.setProperty('--filter-value', value);
+};
+
+const updateSlider = ({MIN: min, MAX: max, STEP: step}) => {
+  const options = {
+    start: max,
+    step: step,
+    connect: 'lower',
+    range: {
+      min: min,
+      max: max,
+    },
+  };
+
+  if (slider.noUiSlider) {
+    slider.noUiSlider.updateOptions(options);
+  } else {
+    noUiSlider.create(slider, options);
+    slider.noUiSlider.on('update', sliderUpdateHandler);
+  }
+};
+
+effectsElements.addEventListener('change', (evt) => {
+  photo.className = '';
+  photo.classList.add(`effects__preview--${evt.target.value}`);
+
+  switch (evt.target.value) {
+    case 'none':
+      if (slider.noUiSlider) {
+        slider.noUiSlider.destroy();
+      }
+      break;
+    case 'chrome':
+      updateSlider(FilterOptions.Chrome);
+      break;
+    case 'sepia':
+      updateSlider(FilterOptions.Sepia);
+      break;
+    case 'marvin':
+      updateSlider(FilterOptions.Marvin);
+      break;
+    case 'phobos':
+      updateSlider(FilterOptions.Phobos);
+      break;
+    case 'heat':
+      updateSlider(FilterOptions.Heat);
+      break;
+  }
+});
 
 const changeScalePhoto = (currentScale, selectedScale) => {
   if (currentScale !== selectedScale) {
@@ -41,7 +127,6 @@ scaleElements.addEventListener('click', (evt) => {
       break;
   }
 });
-
 
 const showEditor = () => {
   document.addEventListener('keydown', escKeydownHandler);
