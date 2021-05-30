898dd1739c0957d9ac7da73ff0d8699a2af2d5f2
srg <skabass@yandex.ru>
2021-05-31 00:01:06 +0300

    Добавлет getData и sendData для работы с сервером

---
+++ js/api.js
@@ -0,0 +1,35 @@
+const SEND_ERROR_MESSAGE = 'Не удалось отправить изображение. Попробуйте еще раз.';
+const GET_ERROR_MESSAGE = 'Не удалось загрузить приложение. Попробуйте позже.';
+
+const getData = (onSuccess, onFall) => {
+  fetch('https://22.javascript.pages.academy/kekstagram/data')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error();
+    })
+    .then((data) => onSuccess(data))
+    .catch(() => onFall(GET_ERROR_MESSAGE));
+};
+
+const sendData = (onSuccess, onFall, body) => {
+  fetch('https://22.javascript.pages.academy/kekstagram', {
+    method: 'POST',
+    type: 'multipart/form-data',
+    body,
+  })
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFall(SEND_ERROR_MESSAGE);
+      }
+    })
+    .catch(() => onFall(SEND_ERROR_MESSAGE));
+};
+
+export {
+  getData,
+  sendData,
+};
