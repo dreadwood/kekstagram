426b2673fe350aaadce0234e2e04344aa321cc0a
srg <skabass@yandex.ru>
2021-06-07 16:25:47 +0300

    Добавляет фильтрацию со встроенной функцией удаления "дребезга"

---
+++ js/filter.js
@@ -0,0 +1,50 @@
+import {makeRandomArr} from './utils.js';
+
+const DEBOUNCE_DELAY = 500;
+const MAX_RANDOM_PHOTOS = 10;
+
+const filter = document.querySelector('.img-filters');
+const filterBtns = filter.querySelectorAll('.img-filters__button');
+
+const showFilter = (data, cb) => {
+  filter.classList.remove('img-filters--inactive');
+
+  let lastTimeout;
+
+  filter.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('img-filters__button')
+      && !evt.target.classList.contains('img-filters__button--active')) {
+      filterBtns.forEach((btn) => {
+        btn.classList.remove('img-filters__button--active');
+        evt.target.classList.add('img-filters__button--active');
+      });
+
+      const sortPhotos = sortPhoto(data, evt.target.id);
+
+      // cb();
+
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = setTimeout(() => cb(sortPhotos), DEBOUNCE_DELAY);
+    }
+  });
+};
+
+const sortPhoto = (dataPhoto, typeSort) => {
+  if (typeSort === 'filter-default') {
+    return dataPhoto;
+  } else if (typeSort === 'filter-random') {
+    return makeRandomArr(dataPhoto).slice(0, MAX_RANDOM_PHOTOS);
+  } else if (typeSort === 'filter-discussed') {
+    return dataPhoto.slice().sort((a, b) => b.comments.length - a.comments.length);
+  }
+};
+
+export {
+  showFilter,
+};
+
+// filter-default
+// filter-random
+// filter-discussed
--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
 import {getData} from './api.js';
 import {renderPhotoFeed} from './feed.js';
+import {showFilter} from './filter.js';
 import {showAppError} from './messages.js';
 import './editor.js';
+// import {debounce} from './utils.js';
 
 // TO-DO
 // переименовать big-picture.js (возможно в post.js)
 // убрать лишние функции из utils.js
 
-// const MAX_NUMBER_PHOTOS = 25;
+// const DEBOUNCE_INTERVAL = 500;
 
-getData(renderPhotoFeed, showAppError);
+getData(
+  (data) => {
+    renderPhotoFeed(data);
+
+    // showFilter(
+    //   data,
+    //   debounce(
+    //     () => renderPhotoFeed(data),
+    //     DEBOUNCE_INTERVAL,
+    //   ),
+    // );
+
+    showFilter(
+      data,
+      renderPhotoFeed,
+    );
+  },
+  showAppError,
+);
